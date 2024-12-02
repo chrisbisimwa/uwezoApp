509e6ddd3f0d4a655b8f4acf88f64fab4e58b1b4
Roland BUHERHWA <rolandbuherhwa@gmail.com>
2024-12-02 07:59:44 +0100

    r10

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     //
+    use HasFactory; 
+
+    protected $table = 'events'; 
+
+    protected $fillable = [ 
+        'title', 
+        'description', 
+        'location', 
+        'start_date',
+        'end_date', 
+        'image_path', 
+        'status' 
+    ]; 
+        protected $dates = [ 
+        'start_date', 
+        'end_date', 
+        'created_at', 
+        'updated_at' 
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(EventCategory::class, 'event_post_categories');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(EventComment::class);
+    }
 }
