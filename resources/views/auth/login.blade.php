8dd61ea078fa6023874de2c50042e26c2cb33a76
Christian BISIMWA <chrisbisimwa@outlook.fr>
2024-12-02 09:54:53 +0200

    Ajout du modèle de page de connexion pour le back-office

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,211 @@
-@extends('back-office.layouts.app')
+@extends('back-office.layouts.app-login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="row authentication mx-0">
 
-                <div class="card-body">
+        <div class="col-xxl-7 col-xl-7 col-lg-12">
+            <div class="row justify-content-center align-items-center h-100">
+                <div class="col-xxl-6 col-xl-7 col-lg-7 col-md-7 col-sm-8 col-12">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="p-5">
+                            <div class="mb-3">
+                                <a href="index.html">
+                                    <img src="{{ asset('back-office-assets/images/brand-logos/desktop-logo.png') }}" alt=""
+                                        class="authentication-brand desktop-logo">
+                                    <img src="{{ asset('back-office-assets/images/brand-logos/desktop-dark.png') }}" alt=""
+                                        class="authentication-brand desktop-dark">
+                                </a>
                             </div>
-                        </div>
+                            <p class="h5 fw-semibold mb-2">Sign In</p>
+                            <div class="btn-list">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                            <div class="text-center my-5 authentication-barrier">
+                            </div>
+                            <div class="row gy-3">
+                                <div class="col-xl-12 mt-0">
+                                    <label for="signin-username"
+                                        class="form-label text-default">{{ __('Email Address') }}</label>
+                                    <input type="text"
+                                        class="form-control form-control-lg @error('email') is-invalid @enderror"
+                                        id="email" type="email" name="email" placeholder="Email Address" required
+                                        autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="col-xl-12 mb-3">
+                                    <label for="signin-password" class="form-label text-default d-block">Password
+                                        @if (Route::has('password.request'))
+                                            <a class="float-end text-danger" href="{{ route('password.request') }}">
+                                                Forget password ?
+                                            </a>
+                                        @endif
+                                        
                                     </label>
+                                    <div class="input-group">
+                                        <input id="password" type="password" name="password"
+                                            class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                            required autocomplete="current-password">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                        <button class="btn btn-light" type="button"
+                                            onclick="createpassword('signin-password',this)" id="button-addon2"><i
+                                                class="ri-eye-off-line align-middle"></i></button>
+                                    </div>
+                                    <div class="mt-2">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                                {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label text-muted fw-normal" for="defaultCheck1">
+                                                Remember password ?
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-12 d-grid mt-2">
+                                    <button type="submit" class="btn btn-lg btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                            <div class="text-center">
+                                <p class="fs-12 text-muted mt-4">Dont have an account? <a href="sign-up-cover.html"
+                                        class="text-primary">Sign Up</a></p>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+        <div class="col-xxl-5 col-xl-5 col-lg-5 d-xl-block d-none px-0">
+            <div class="authentication-cover">
+                <div class="aunthentication-cover-content rounded">
+                    <div
+                        class="swiper keyboard-control swiper-initialized swiper-horizontal swiper-pointer-events swiper-backface-hidden">
+                        <div class="swiper-wrapper" id="swiper-wrapper-ca24d161e7ded101c" aria-live="off"
+                            style="transform: translate3d(-822px, 0px, 0px); transition-duration: 0ms;">
+                            <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next"
+                                data-swiper-slide-index="2" role="group" aria-label="3 / 3"
+                                style="width: 381px; margin-right: 30px;">
+                                <div
+                                    class="text-fixed-white text-center p-5 d-flex align-items-center justify-content-center">
+                                    <div>
+                                        <div class="mb-5">
+                                            <img src="{{ asset('back-office-assets/images/authentication/2.png') }}" class="authentication-image"
+                                                alt="">
+                                        </div>
+                                        <h6 class="fw-semibold">Sign In</h6>
+                                        <p class="fw-normal fs-14 op-7"> Lorem ipsum dolor sit amet, consectetur adipisicing
+                                            elit. Ipsa eligendi expedita aliquam quaerat nulla voluptas facilis. Porro rem
+                                            voluptates possimus, ad, autem quae culpa architecto, quam labore blanditiis at
+                                            ratione.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="0" role="group"
+                                aria-label="1 / 3" style="width: 381px; margin-right: 30px;">
+                                <div
+                                    class="text-fixed-white text-center p-5 d-flex align-items-center justify-content-center">
+                                    <div>
+                                        <div class="mb-5">
+                                            <img src="{{ asset('back-office-assets/images/authentication/2.png') }}" class="authentication-image"
+                                                alt="">
+                                        </div>
+                                        <h6 class="fw-semibold">Sign In</h6>
+                                        <p class="fw-normal fs-14 op-7"> Lorem ipsum dolor sit amet, consectetur adipisicing
+                                            elit. Ipsa eligendi expedita aliquam quaerat nulla voluptas facilis. Porro rem
+                                            voluptates possimus, ad, autem quae culpa architecto, quam labore blanditiis at
+                                            ratione.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="1" role="group"
+                                aria-label="2 / 3" style="width: 381px; margin-right: 30px;">
+                                <div
+                                    class="text-fixed-white text-center p-5 d-flex align-items-center justify-content-center">
+                                    <div>
+                                        <div class="mb-5">
+                                            <img src="{{ asset('back-office-assets/images/authentication/3.png') }}" class="authentication-image"
+                                                alt="">
+                                        </div>
+                                        <h6 class="fw-semibold">Sign In</h6>
+                                        <p class="fw-normal fs-14 op-7"> Lorem ipsum dolor sit amet, consectetur
+                                            adipisicing
+                                            elit. Ipsa eligendi expedita aliquam quaerat nulla voluptas facilis. Porro rem
+                                            voluptates possimus, ad, autem quae culpa architecto, quam labore blanditiis at
+                                            ratione.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide swiper-slide-next" data-swiper-slide-index="2" role="group"
+                                aria-label="3 / 3" style="width: 381px; margin-right: 30px;">
+                                <div
+                                    class="text-fixed-white text-center p-5 d-flex align-items-center justify-content-center">
+                                    <div>
+                                        <div class="mb-5">
+                                            <img src="{{ asset('back-office-assets/images/authentication/2.png') }}" class="authentication-image"
+                                                alt="">
+                                        </div>
+                                        <h6 class="fw-semibold">Sign In</h6>
+                                        <p class="fw-normal fs-14 op-7"> Lorem ipsum dolor sit amet, consectetur
+                                            adipisicing elit. Ipsa eligendi expedita aliquam quaerat nulla voluptas facilis.
+                                            Porro rem voluptates possimus, ad, autem quae culpa architecto, quam labore
+                                            blanditiis at ratione.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev"
+                                data-swiper-slide-index="0" role="group" aria-label="1 / 3"
+                                style="width: 381px; margin-right: 30px;">
+                                <div
+                                    class="text-fixed-white text-center p-5 d-flex align-items-center justify-content-center">
+                                    <div>
+                                        <div class="mb-5">
+                                            <img src="{{ asset('back-office-assets/images/authentication/2.png') }}" class="authentication-image"
+                                                alt="">
+                                        </div>
+                                        <h6 class="fw-semibold">Sign In</h6>
+                                        <p class="fw-normal fs-14 op-7"> Lorem ipsum dolor sit amet, consectetur
+                                            adipisicing elit. Ipsa eligendi expedita aliquam quaerat nulla voluptas facilis.
+                                            Porro rem voluptates possimus, ad, autem quae culpa architecto, quam labore
+                                            blanditiis at ratione.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-button-next" tabindex="0" role="button" aria-label="Next slide"
+                            aria-controls="swiper-wrapper-ca24d161e7ded101c"></div>
+                        <div class="swiper-button-prev" tabindex="0" role="button" aria-label="Previous slide"
+                            aria-controls="swiper-wrapper-ca24d161e7ded101c"></div>
+                        <div
+                            class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal">
+                            <span class="swiper-pagination-bullet" tabindex="0" role="button"
+                                aria-label="Go to slide 1"></span><span
+                                class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0"
+                                role="button" aria-label="Go to slide 2" aria-current="true"></span><span
+                                class="swiper-pagination-bullet" tabindex="0" role="button"
+                                aria-label="Go to slide 3"></span>
+                        </div>
+                        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-</div>
+
+
+
+
+
+
+
+    
 @endsection
---
+++ resources/views/back-office/layouts/app-login.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html dir="ltr" data-nav-layout="vertical" data-theme-mode="light" data-header-styles="light" data-menu-styles="dark"
+    data-vertical-style="overlay" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+    <!-- Meta Data -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>UWEZO APP</title>
+    <meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
+    <meta name="Author" content="KivuTech">
+    <meta name="keywords"
+        content="admin,admin dashboard,admin panel,admin template,bootstrap,clean,dashboard,flat,jquery,modern,responsive,premium admin templates,responsive admin,ui,ui kit.">
+
+    <link rel="icon" href="{{ asset('back-office-assets/images/brand-logos/favicon.ico') }}" type="image/x-icon">
+
+    <script src="{{ asset('back-office-assets/js/authentication-main.js') }}"></script>
+    <link id="style" href="{{ asset('back-office-assets/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('back-office-assets/css/styles.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('back-office-assets/css/icons.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('back-office-assets/libs/swiper/swiper-bundle.min.css') }}">
+
+
+
+    @livewireStyles
+</head>
+
+
+<body class="bg-white">
+
+
+
+    @yield('content')
+
+
+
+
+
+
+
+    @livewireScripts
+
+    <script src="{{ asset('back-office-assets/js/custom-switcher.min.js') }}"></script>
+    <script src="{{ asset('back-office-assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('back-office-assets/libs/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('back-office-assets/js/authentication.js') }}"></script>
+    <script src="{{ asset('back-office-assets/js/show-password.js') }}"></script>
+
+
+
+    <script src="//unpkg.com/alpinejs" defer></script>
+    @if (session()->has('success'))
+        <script>
+            var notyf = new Notyf({
+                dismissible: true
+            })
+            notyf.success('{{ session('success') }}')
+        </script>
+    @endif
+
+
+
+
+
+    <script>
+        /* Simple Alpine Image Viewer */
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('imageViewer', (src = '') => {
+                return {
+                    imageUrl: src,
+
+                    refreshUrl() {
+                        this.imageUrl = this.$el.getAttribute("image-url")
+                    },
+
+                    fileChosen(event) {
+                        this.fileToDataUrl(event, src => this.imageUrl = src)
+                    },
+
+                    fileToDataUrl(event, callback) {
+                        if (!event.target.files.length) return
+
+                        let file = event.target.files[0],
+                            reader = new FileReader()
+
+                        reader.readAsDataURL(file)
+                        reader.onload = e => callback(e.target.result)
+                    },
+                }
+            })
+        })
+    </script>
+
+
+
+</body>
+
+</html>
