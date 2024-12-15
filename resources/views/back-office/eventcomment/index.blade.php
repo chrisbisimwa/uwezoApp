c4a915b13f973c228c2eef93d4ff40b95e6aedd3
Roland BUHERHWA <rolandbuherhwa@gmail.com>
2024-12-15 13:22:42 +0100

    event comment

---
+++ resources/views/back-office/eventcomment/index.blade.php
@@ -0,0 +1,17 @@
+@extends('back-office.layouts.app')
+
+@section('content')
+<div class="d-md-flex d-block align-items-center justify-content-between my-4 page-header-breadcrumb">
+    <h1 class="page-title fw-semibold fs-18 mb-0">Liste commentaires</h1>
+    <div class="ms-md-1 ms-0">
+        <nav>
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a href="#">Evènement</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Liste des commentaires</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+            @livewire('back-office.eventcomment.index')
+
+@endsection
