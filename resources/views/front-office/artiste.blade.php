fe6348f47508e752f3812659a2159d18351b52aa
Patrick Baraka <68458239+patrick-git-max@users.noreply.github.com>
2024-12-17 08:56:58 +0200

    gauchoArtiste

--- resources/views/front-office/artiste.blade.php
+++ resources/views/front-office/artiste.blade.php
@@ -1,5 +1,375 @@
 @extends("front-office.layouts.app")
 
-@section("content")
 
+@section("content")
+<div class="andro_subheader bg-cover bg-center bg-norepeat" style="background-image: url(front-office-assets/img/subheader-1.jpg)">
+    
+  </div>
+<div class="section md white-bg">
+    <div class="container">
+
+      <div class="andro_isotope-filter-nav">
+        <a href="#" class="andro_isotope-trigger active" data-filter="*">Toutes les categories</a>
+        <a href="#" class="andro_isotope-trigger" data-filter=".d1">Cat 1</a>
+        <a href="#" class="andro_isotope-trigger" data-filter=".d2">Cat 2</a>
+        <a href="#" class="andro_isotope-trigger" data-filter=".d3">Cat 3</a>
+      </div>
+
+      <div class="row andro_isotope-filter" style="position: relative; height: 2981.42px;">
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d1" style="position: absolute; left: 0px; top: 0px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/1.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Audio Engineer</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Stacey Gold</a> </h5>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d2" style="position: absolute; left: 0px; top: 372.677px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/2.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Pianist</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Dacey Arbit</a> </h5>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d3" style="position: absolute; left: 0px; top: 745.354px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/3.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Drum Artist</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Jakki Walim</a> </h5>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d1 d3" style="position: absolute; left: 0px; top: 1118.03px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/4.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Guitarist</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Noimi Smith</a> </h5>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d2" style="position: absolute; left: 0px; top: 1490.71px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/5.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Audio Engineer</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Stacey Gold</a> </h5>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d3" style="position: absolute; left: 0px; top: 1863.38px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/6.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Pianist</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Dacey Arbit</a> </h5>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d1" style="position: absolute; left: 0px; top: 2236.06px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/7.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Drum Artist</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Jakki Walim</a> </h5>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-4 col-sm-6 andro_isotope-item d2" style="position: absolute; left: 0px; top: 2608.74px;">
+          <div class="andro_artist style-2">
+            <div class="andro_artist-img">
+              <img src="front-office-assets/img/artists/md/8.jpg" alt="artist">
+              <div class="andro_artist-img-content">
+                <div class="andro_artist-meta">
+                  <span>First Day: </span>
+                  <b>08:00AM - 05:00PM</b>
+                </div>
+                <ul class="andro_socials">
+                  <li> <a class="facebook" href="#"> <i class="fab fa-facebook-f"></i> </a> </li>
+                  <li> <a class="instagram" href="#"> <i class="fab fa-instagram"></i> </a> </li>
+                  <li> <a class="soundcloud" href="#"> <i class="fab fa-soundcloud"></i> </a> </li>
+                </ul>
+              </div>
+            </div>
+            <span class="andro_artist-designation">Guitarist</span>
+            <h5 class="andro_artist-name"> <a href="artist-details.html">Noimi Smith</a> </h5>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div class="section light-bg __web-inspector-hide-shortcut__">
+    <div class="container">
+
+      <div class="section-title text-center">
+        <h3>Find all The Top Music &amp; Trending Artists</h3>
+        <p class="mb-0">Music and playlists will let your fans enjoy listening</p>
+      </div>
+
+      <div class="andro_lineup">
+
+        <div class="andro_lineup-item">
+
+          <div class="andro_lineup-title">
+            <h5 class="h3 color-primary">Experimental</h5>
+          </div>
+          <div class="andro_lineup-table">
+            <div class="andro_lineup-table-row">
+              <b>Deyonce</b>
+              <b>Starly Rae</b>
+              <b>Prank Frocean</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>Grimes</b>
+              <b>D'angle</b>
+              <b>Erykah Bui</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>The Weekday</b>
+              <b>Miguel</b>
+              <a href="#" class="btn-link">View all <i class="fal fa-arrow-right"></i> </a>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="andro_lineup-item">
+
+          <div class="andro_lineup-title">
+            <h5 class="h3 color-primary">Rock</h5>
+          </div>
+          <div class="andro_lineup-table">
+            <div class="andro_lineup-table-row">
+              <b>Deyonce</b>
+              <b>Starly Rae</b>
+              <b>Prank Frocean</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>Grimes</b>
+              <b>D'angle</b>
+              <b>Erykah Bui</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>The Weekday</b>
+              <b>Miguel</b>
+              <a href="#" class="btn-link">View all <i class="fal fa-arrow-right"></i> </a>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="andro_lineup-item">
+
+          <div class="andro_lineup-title">
+            <h5 class="h3 color-primary">R&amp;B</h5>
+          </div>
+          <div class="andro_lineup-table">
+            <div class="andro_lineup-table-row">
+              <b>Deyonce</b>
+              <b>Starly Rae</b>
+              <b>Prank Frocean</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>Grimes</b>
+              <b>D'angle</b>
+              <b>Erykah Bui</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>The Weekday</b>
+              <b>Miguel</b>
+              <a href="#" class="btn-link">View all <i class="fal fa-arrow-right"></i> </a>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="andro_lineup-item">
+
+          <div class="andro_lineup-title">
+            <h5 class="h3 color-primary">Hip Hop</h5>
+          </div>
+          <div class="andro_lineup-table">
+            <div class="andro_lineup-table-row">
+              <b>Deyonce</b>
+              <b>Starly Rae</b>
+              <b>Prank Frocean</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>Grimes</b>
+              <b>D'angle</b>
+              <b>Erykah Bui</b>
+            </div>
+            <div class="andro_lineup-table-row">
+              <b>The Weekday</b>
+              <b>Miguel</b>
+              <a href="#" class="btn-link">View all <i class="fal fa-arrow-right"></i> </a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+  <div class="section lg blue-bg" style="background-image: url(assets/img/texture-2.png)">
+    <div class="container">
+      <div class="row justify-content-center">
+
+        <div class="col-lg-4 col-md-6">
+          <div class="andro_infobox">
+            <div class="andro_infobox-icon">
+              <i class="flaticon-call"></i>
+            </div>
+            <div class="andro_infobox-body">
+              <h6 class="text-white">Phone</h6>
+              <span class="text-white">+(123)4567890</span>
+              <span class="text-white">+(123)4567890</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+          <div class="andro_infobox">
+            <div class="andro_infobox-icon">
+              <i class="flaticon-mail"></i>
+            </div>
+            <div class="andro_infobox-body">
+              <h6 class="text-white">Email</h6>
+              <span class="text-white">example@example.com</span>
+              <span class="text-white">example@example.com</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+          <div class="andro_infobox">
+            <div class="andro_infobox-icon">
+              <i class="flaticon-pin"></i>
+            </div>
+            <div class="andro_infobox-body">
+              <h6 class="text-white">Location</h6>
+              <span class="text-white">123 New Yourk E Block 12670,</span>
+              <span class="text-white">Street 2101 USA</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="andro_instagram d-none d-lg-block container mb-negative lg">
+      <a href="#"> <i class="fab fa-instagram"></i> </a>
+      <div class="row gx-0">
+        <div class="col-lg-2 col-md-3 col-sm-6">
+          <a href="#"><img src="assets/img/instagram/1.jpg" alt="instagram"></a>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6">
+          <a href="#"><img src="assets/img/instagram/2.jpg" alt="instagram"></a>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6">
+          <a href="#"><img src="assets/img/instagram/3.jpg" alt="instagram"></a>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6">
+          <a href="#"><img src="assets/img/instagram/4.jpg" alt="instagram"></a>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6">
+          <a href="#"><img src="assets/img/instagram/5.jpg" alt="instagram"></a>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-6">
+          <a href="#"><img src="assets/img/instagram/6.jpg" alt="instagram"></a>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
