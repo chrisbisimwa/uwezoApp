497352e426a224b3bc5b55544fb10fe74ed2f3fe
Christian BISIMWA <chrisbisimwa@outlook.fr>
2025-01-22 10:19:59 +0200

    Ajout de la prise en charge de PWA avec un service worker et une page hors ligne

--- resources/views/front-office/layouts/app.blade.php
+++ resources/views/front-office/layouts/app.blade.php
@@ -38,6 +38,8 @@
 
 
     @livewireStyles
+
+    @laravelPWA
 </head>
 
 
@@ -131,6 +133,21 @@
         })
     </script>
 
+
+    <script type="text/javascript">
+        // Initialize the service worker
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/serviceworker.js', {
+                scope: '.'
+            }).then(function(registration) {
+                // Registration was successful
+                console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                // registration failed :(
+                console.log('Laravel PWA: ServiceWorker registration failed: ', err);
+            });
+        }
+    </script>
 
 
 </body>
