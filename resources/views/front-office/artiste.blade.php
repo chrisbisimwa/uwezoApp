9091ffdbce6bdaff0120e0ee09d88ec65dbef098
Christian BISIMWA <chrisbisimwa@outlook.fr>
2025-02-04 14:25:11 +0200

    Suppression de commentaires inutiles dans le fichier artiste.blade.php

--- resources/views/front-office/artiste.blade.php
+++ resources/views/front-office/artiste.blade.php
@@ -39,7 +39,7 @@
         </div>
     </div>
     
-    {{-- <div class="section light-bg __web-inspector-hide-shortcut__">
+    <div class="section light-bg __web-inspector-hide-shortcut__">
         <div class="container">
 
             <div class="section-title text-center">
@@ -54,8 +54,8 @@
             </div>
 
         </div>
-    </div> --}}
-    <div class="section lg blue-bg" style="background-image: url(assets/img/texture-2.png)">
+    </div>
+   {{--  <div class="section lg blue-bg" style="background-image: url(assets/img/texture-2.png)">
         <div class="container">
             <div class="row justify-content-center">
 
@@ -123,5 +123,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
